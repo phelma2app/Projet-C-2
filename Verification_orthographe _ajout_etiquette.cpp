ca2f6d2a5389b899de5b9f0c5f00ea1210dccae4
phelma2app <31961914+phelma2app@users.noreply.github.com>
2017-10-08 11:46:58 +0200

    Update Verification_orthographe _ajout_etiquette.cpp

--- Verification_orthographe _ajout_etiquette.cpp
+++ Verification_orthographe _ajout_etiquette.cpp
@@ -1,36 +1,91 @@
-#include "Lexeme.h"
-#include <string>
 #include <iostream>
+#include <string>
+#include <list>
+#include "utilities.cpp"  // fichier contenant les fonctions is_number, is_letter, is_ponctuation, is_ponctuation2char
 
-grdjojh<fauiuyuiyui
+using namespace std ; 
 
-using namespace std;
-
-Lexeme::~Lexeme() {
-	// TODO Auto-generated destructor stub
+bool verif(Lexeme l, bool *com, int *ligne_com) {  
+	string a = l.getLex() ;
+	int i =0 ; 
+	bool erreur = false ;
+if (*com == true && *ligne_com == l.getLigne()){ 	// ici on est dans le cas ou on a eu la ponctuation commentaire, si on est toujours sur la 
+	l.isCommentaire() ; 				//meme ligne que ce dernier on place le lexeme en commentaire et on ne fait rien d'autre
+	return erreur ; 
 }
+else {							// SInon on teste le lexeme pour vérifier l'orthographe et mettre la bonne etiquette 
+	*com = false ;
+	if (is_letter(a[0])){
+		l.isMot() ;
+		i++;
+		while (a[i]!='\0'){
+			if (is_letter(a[0])||is_number(a[0])||a[i]=='_'){}
+			else { cout << "ERREUR ligne " << l.getLigne() << ": Un mot commencant par une lettre peut contenir seulement des lettres, des chiffres ou un underscore" << endl ; 
+			erreur = true ; 
+			}
+			i++ ; 
+		}
+		if (a[i-1]=='_'){
+		cout << "ERREUR ligne " << l.getLigne() << " : un mot commencant par une lettre ne peut pas se terminer par le caractere '_'" << endl ; 
+		//cout numero de la ligne du lexème
+		erreur = true ; 
+		}
+	}
+	else if (is_number(a[0])){
+		l.isNombre() ; 
+		i++ ; 
+		while (a[i]!='\0'){
+			if(is_number(a[i])){}
+			else {
+				cout << "ERREUR ligne " << l.getLigne() << ": Un mot commencant par un chiffre peut contenir seulement des chiffres" << endl ; 
+				//cout numero de la ligne du lexème 
+			 	erreur = true ; 
+			}
+			i=i++ ; 
+		}
+	}
+	else if (is_ponctuation(a[0])){
+		if (a[1]=='\0'){
+			l.isPonctuation() ; 
+		}
+		else if (is_ponctuation(a[1])){
+			if(is_ponctuation2char(a[0],a[1])){
+				if (a[0]=='-'&&a[0]=='-'){
+					l.isCommentaire() ;
+					*ligne_com = l.getligne ;  
+					*com = TRUE ;
+				}
+				else {
+					l.isPonctuation() ;
+				}
+			}
+			else {
+				cout << "ERREUR ligne " << l.getLigne() << ": La double ponctuation n'existe pas" << endl ; 
+				//cout numero de la ligne 
+				erreur = true ; 
+			}
+			if(a[2]!='\0'){
+			cout << "ERREUR ligne " << l.getLigne() << " : On ne peut pas avoir plus de 2 ponctuations à la suite" << endl ;
+			// + n ligne
+			erreur = true ; 
+			} 
+		}
+		else {
+			cout << "ERREUR ligne " << l.getLigne() << " : On ne peut pas avoir d'autres caractères qu'une ponctuation après une ponctuation" << endl ; 
+			// + n ligne 
+			erreur = true ; 
+		}
 
-string Lexeme::afficheType() const
-{
-	switch(type_lex)
-	{
-		case PONCTUATION :
-			return "Ponctuation";
-		case MOT :
-			return "Mot";
-		case NOMBRE :
-			return "Nombre";
-		case COMMENTAIRE :
-			return "Commentaire";
-		default :
-			return "Non Defini";
 	}
 }
-
-ostream& operator << (ostream& out, Lexeme l)
-{
-	out << l.getLex() << "	|	";
-	out << l.getLigne() << "	|	";
-	out << l.afficheType() << endl;
-	return out;
+	return erreur ; 
 }
+
+/*
+list<Lexeme *>::iterator itr
+bool com = FALSE ;
+for(itr = l.begin() ; itr != l.end() ; ++itr) 
+{
+	Lexemes* a=*itr ;
+	
+}*/
