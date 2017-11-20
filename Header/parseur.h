a75284fec6e121658381f209dd0431c4de0f26a2
phelma2app <31961914+phelma2app@users.noreply.github.com>
2017-11-20 10:05:15 +0100

    Update parseur.h

--- Header/parseur.h
+++ Header/parseur.h
@@ -3,6 +3,14 @@
 
 using namespace std;
 
-int parseur_entity (int i) ; // ici i correspond au numero de la case de notre liste 
+int parseur_entity (list<Lexeme*>::iterator itr) ; 
 
-int parseur_port (int i ) ; 
+int parseur_port (list<Lexeme*>::iterator itr) ; 
+
+int parseur_type_port (list<Lexeme*>::iterator itr, bool *fin_port);
+
+int parseur_library (list<Lexeme*>::iterator itr) ;
+
+int parseur_use (list<Lexeme*>::iterator itr, string library);
+
+int parseur_architecture (list<Lexeme*>::iterator itr);
