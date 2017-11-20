5d1f22afd99fca57fe70307e51c5a76d2a2b2fbd
phelma2app <31961914+phelma2app@users.noreply.github.com>
2017-11-20 10:10:32 +0100

    Create Verification_orthographe_ajout_etiquette.h

---
+++ Header/Verification_orthographe_ajout_etiquette.h
@@ -0,0 +1,14 @@
+#ifndef VERIFICATION_ORTHOGRAPHE_AJOUT_ETIQUETTE_H_
+#define VERIFICATION_ORTHOGRAPHE_AJOUT_ETIQUETTE_H_
+
+#include <iostream>
+#include <string>
+#include <list>
+#include "utilities.h"  // fichier contenant les fonctions is_number, is_letter, is_ponctuation, is_ponctuation2char
+#include "Lexeme.h"
+
+using namespace std ; 
+
+bool verif(Lexeme l, bool *com, int *ligne_com);
+
+#endif //VERIFICATION_ORTHOGRAPHE_AJOUT_ETIQUETTE_H_
