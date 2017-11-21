682a0c3cd351ec18760d57f118103e5c5722e1c8
phelma2app <31961914+phelma2app@users.noreply.github.com>
2017-11-21 11:58:19 +0100

    Update parseur.h

--- Header/parseur.h
+++ Header/parseur.h
@@ -14,3 +14,21 @@ int parseur_library (list<Lexeme*>::iterator itr) ;
 int parseur_use (list<Lexeme*>::iterator itr, string library);
 
 int parseur_architecture (list<Lexeme*>::iterator itr);
+
+int parseur_signal (list<Lexeme*>::iterator itr) ; 
+
+int parseur_component (list<Lexeme*>::iterator itr);
+
+int parseur_generic (list<Lexeme*>::iterator itr) ;
+
+int parseur_process (list<Lexeme*>::iterator itr);
+
+int parseur_type (list<Lexeme*>::iterator itr);
+
+int parseur_if (list<Lexeme*>::iterator itr);
+
+int parseur_then (list<Lexeme*>::iterator itr);
+
+int parseur_else (list<Lexeme*>::iterator itr);
+
+int parseur_elsif (list<Lexeme*>::iterator itr);
