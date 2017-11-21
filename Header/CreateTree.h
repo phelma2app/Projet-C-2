f4211b37bb73389bbe12a1880e745f163165b4a9
phelma2app <31961914+phelma2app@users.noreply.github.com>
2017-11-21 12:31:15 +0100

    Create CreateTree.h

---
+++ Header/CreateTree.h
@@ -0,0 +1,14 @@
+#ifndef CREATETREE_h_
+#define CREATETREE_h_
+
+#include "tree.hh"
+#include "Lexeme.h"
+#include <algorithm>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void printTree(tree<Lexeme*> tr);
+
+#endif //CREATETREE_h_
