16b393fef6085882cebde6a0b825b72783f2fb66
phelma2app <31961914+phelma2app@users.noreply.github.com>
2017-11-21 12:28:46 +0100

    Create Console.cpp

---
+++ Source/Console.cpp
@@ -0,0 +1,47 @@
+#include "../Header/Console.h"
+
+using namespace std;
+
+//Commandes sur la console pour l'utilisateur
+//Retourne un entier qui vaut 0 si les commandes ont Ã©tÃ© comprises
+int commande_vhdl(string &libraryname, string &sourcename)
+{
+	string commande;
+	cin >> commande;
+
+	//Commande utilisateur
+	if(commande=="vhdlcomp")
+	{
+		if(commande_vhdlcomp(libraryname, sourcename)!=0)
+		{
+			return 1;
+		}
+	}
+	else
+	{
+		cout << "Erreur : commande inconnue" << endl;
+		return ERREUR;
+	}
+	return ERREUR;
+}
+
+//Commande VHDLCOMP
+//
+int commande_vhdlcomp(string &libraryname, string &sourcename)
+{
+	cout << "On va compiler" << endl;	//pour test
+	string commande;
+	cin >> commande;
+	if(commande=="-l")
+	{
+		cout << "Le nom de la librarie est donnee" << endl;	//pour test
+		cin >> libraryname;
+		cin >> sourcename;
+	}
+	else
+	{
+		cout << "Le nom de la librairie n'est pas donnee" << endl; //pour test
+		sourcename=commande;
+	}
+	return VHDLCOMP;
+}
