fc2a5fe72d1b195e68f422c74f512fe6dc380a7f
phelma2app <31961914+phelma2app@users.noreply.github.com>
2017-11-24 12:55:09 +0100

    Update parseur.h

--- Header/parseur.h
+++ Header/parseur.h
@@ -5,32 +5,33 @@ using namespace std;
 
 int parseur_root (list<Lexeme*>& list_lex);
 
-int parseur_entity (list<Lexeme*>::iterator itr) ; 
+int parseur_entity (list<Lexeme*>::iterator& itr) ; 
 
-int parseur_port (list<Lexeme*>::iterator itr) ; 
+int parseur_port (list<Lexeme*>::iterator& itr) ; 
 
-int parseur_type_port (list<Lexeme*>::iterator itr, bool *fin_port);
+int parseur_type_port (list<Lexeme*>::iterator& itr, bool *fin_port);
 
-int parseur_library (list<Lexeme*>::iterator itr) ;
+int parseur_library (list<Lexeme*>::iterator& itr) ;
 
-int parseur_use (list<Lexeme*>::iterator itr, string library);
+int parseur_use (list<Lexeme*>::iterator& itr, string library);
 
-int parseur_architecture (list<Lexeme*>::iterator itr);
+int parseur_architecture (list<Lexeme*>::iterator& itr);
 
-int parseur_signal (list<Lexeme*>::iterator itr) ; 
+int parseur_signal (list<Lexeme*>::iterator& itr) ; 
 
-int parseur_component (list<Lexeme*>::iterator itr);
+int parseur_component (list<Lexeme*>::iterator& itr);
 
-int parseur_generic (list<Lexeme*>::iterator itr) ;
+int parseur_generic (list<Lexeme*>::iterator& itr) ;
 
-int parseur_process (list<Lexeme*>::iterator itr);
+int parseur_process (list<Lexeme*>::iterator& itr);
 
-int parseur_type (list<Lexeme*>::iterator itr);
+int parseur_type (list<Lexeme*>::iterator& itr);
 
-int parseur_if (list<Lexeme*>::iterator itr);
+int parseur_if (list<Lexeme*>::iterator& itr);
 
-int parseur_then (list<Lexeme*>::iterator itr);
+int parseur_then (list<Lexeme*>::iterator& itr);
 
-int parseur_else (list<Lexeme*>::iterator itr);
+int parseur_else (list<Lexeme*>::iterator& itr);
 
-int parseur_elsif (list<Lexeme*>::iterator itr);
+int parseur_elsif (list<Lexeme*>::iterator& itr);
+
