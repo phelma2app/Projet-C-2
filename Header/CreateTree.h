8c77f078ac8022e1250f2d3c17f0fe490d707a54
phelma2app <31961914+phelma2app@users.noreply.github.com>
2017-11-24 12:56:00 +0100

    Update CreateTree.h

--- Header/CreateTree.h
+++ Header/CreateTree.h
@@ -13,11 +13,16 @@ using namespace std;
 
 tree<Lexeme*> createTree(list<Lexeme*>& l);
 
+//------------------------------------Entity--------------------------------
 void constructTreeOnEntity(tree<Lexeme*>& tr, tree<Lexeme*>::pre_order_iterator& entity, list<Lexeme*>& l, list<Lexeme*>::iterator begin_entity);
 void constructTreeOnEntityID(tree<Lexeme*>& tr, tree<Lexeme*>::pre_order_iterator& entityid, list<Lexeme*>& l, list<Lexeme*>::iterator begin_entityid);
 void constructTreeOnPort(tree<Lexeme*>& tr, tree<Lexeme*>::pre_order_iterator& port, list<Lexeme*>& l, list<Lexeme*>::iterator begin_port);
 void constructTreeOnPortId(tree<Lexeme*>& tr, tree<Lexeme*>::pre_order_iterator& portid, list<Lexeme*>& l, list<Lexeme*>::iterator begin_portid);
 
+//------------------------------------Library--------------------------------
+void constructTreeOnLibrary(tree<Lexeme*>& tr, tree<Lexeme*>::pre_order_iterator& library, list<Lexeme*>& l, list<Lexeme*>::iterator begin_library);
+
+//------------------------------------Print--------------------------------
 void printTree(tree<Lexeme*> tr);
 
 #endif //CREATETREE_h_
