7d8ccd26ee8ca5ba2e98ff6d9e4aba1c4eea8b18
phelma2app <31961914+phelma2app@users.noreply.github.com>
2017-11-24 11:06:48 +0100

    Update Lexeme.cpp

--- Source/Lexeme.cpp
+++ Source/Lexeme.cpp
@@ -12,14 +12,40 @@ string Lexeme::afficheType() const
 {
 	switch(type_lex)
 	{
-		case PONCTUATION :
-			return "Ponctuation";
+		case ARCHITECTURE :
+			return "Architecture";		
+		case COMMENTAIRE :
+			return "Commentaire";
+		case ENTITY :
+			return "Entity";
+		case ENTITY_END :
+			return "End Entity";
+		case ENTITY_ID :
+			return "Identifiant Entity";
+		case LIBRARY :
+			return "Library";
 		case MOT :
 			return "Mot";
 		case NOMBRE :
 			return "Nombre";
-		case COMMENTAIRE :
-			return "Commentaire";
+		case PONCTUATION :
+			return "Ponctuation";
+		case PORT :
+			return "Port";
+		case PORT_END:
+			return "End Port";
+		case PORT_ID:
+			return "Identifiant Port";
+		case PORT_ID_END:
+			return "End Identifiant Port";
+		case PORT_IN:
+			return "Port In";
+		case PORT_INOUT :
+			return "Port In/Out";
+		case PORT_OUT:
+			return "Port Out";
+		case PORT_TYPE:
+			return "Type Port";
 		default :
 			return "Non Defini";
 	}
