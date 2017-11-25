38911229bdae64a8b4d2ad43c2f3463580837371
phelma2app <31961914+phelma2app@users.noreply.github.com>
2017-11-25 15:37:21 +0100

    Create entite_noarchi.vhd

---
+++ Tests/Test_chamilo/entite_noarchi.vhd
@@ -0,0 +1,19 @@
+
+library STD;
+  use STD.textio.all;
+library IEEE;
+  use IEEE.bit_1164.all;
+  use IEEE.bit_misc.all;
+  use IEEE.bit_arith.all;
+
+entity entite_noarchi is
+  Port(    
+    CLK :       In    bit;
+    RST :       In   bit;
+    start :      In  bit;
+    done :      In  bit;
+    DIN :       In    bit_vector(15 downto 0);
+    DOUT :       Out   bit_vector(15 downto 0)
+
+    );
+  end entite_noarchi;
