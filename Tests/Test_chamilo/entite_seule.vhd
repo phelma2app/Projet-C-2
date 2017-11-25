e1d12a9045d048d4d50f29a726e0cbeb8e0e9b9b
phelma2app <31961914+phelma2app@users.noreply.github.com>
2017-11-25 15:37:51 +0100

    Create entite_seule.vhd

---
+++ Tests/Test_chamilo/entite_seule.vhd
@@ -0,0 +1,9 @@
+library STD;
+  use STD.textio.all;
+library IEEE;
+  use IEEE.bit_1164.all;
+  use IEEE.bit_misc.all;
+  use IEEE.bit_arith.all;
+
+entity entite_sans_portes is
+ end entite_sans_portes;
