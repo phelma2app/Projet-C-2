51733dfd4e5f418a7c6691b68a5b25b7e453c4f2
phelma2app <31961914+phelma2app@users.noreply.github.com>
2017-11-27 10:18:13 +0100

    Update Lexeur.cpp

--- Source/Lexeur.cpp
+++ Source/Lexeur.cpp
@@ -6,22 +6,23 @@ int vhdlcomp(string libraryname, string sourcename, list<Lexeme*>& list_lex)
 {
 	//Ouverture du fichier
 	ifstream fichier(sourcename.c_str(), ios::in);  // on ouvre en lecture
-	if(!fichier)  // si l'ouverture n'a pas fonctionnÃÂ©e
+	if(!fichier)  // si l'ouverture n'a pas fonctionnee
 	{
 		cerr << "Impossible d'ouvrir le fichier " << sourcename << " !" << endl;
 		return 1;
 	}
 
 	//Traitement du fichier
-	int n_ligne=0;
+	int n_ligne=1;
 	do
 	{
 		string buffer ="";
 		buffer = read_line(fichier);	//on lit une ligne
-		stock_lexemes(buffer,n_ligne,list_lex);		//on sÃÂ©pare les lexemes
+		cout << "Stock_Lexeme : ligne nÂ°" << n_ligne << endl;
+		stock_lexemes(buffer,n_ligne,list_lex);		//on separe les lexemes
 		n_ligne++;
 	}while(!fichier.eof());
-
+	cout << "Fin du fichier atteint" << endl;
 
 	print_lex(list_lex);
 	cout << endl <<  "Nombre de lignes dans le fichier : " << n_ligne-1 << endl;		//pour test
@@ -38,17 +39,17 @@ void print_lex(list<Lexeme*>& list_lex)
 
 }
 
-//Fonction pour stocker chaque lexÃÂ¨me d'une ligne dans une liste, avec leur numÃÂ©ro de ligne
-//PrÃÂ©-condition : les lexÃÂ¨mes doivent ÃÂªtre sÃÂ©parÃÂ©s avec des espaces
+//Fonction pour stocker chaque lexeme d'une ligne dans une liste, avec leur numero de ligne
+//Pre-condition : les lexemes doivent etre separes avec des espaces
 void stock_lexemes(string buffer, int ligne, list<Lexeme*>& Lex_VHDL)
 {
 	char* ch_buff;	//Buffer
 	ch_buff = strtok(&buffer[0]," ");
 	int ligne_com=0;
 	bool is_com=false;
-	while(ch_buff!=NULL)
+	while(ch_buff!=NULL||ch_buff!='\0')
 	{
-		//	cout << ch_buff << endl;	//pour test
+		cout << "." << ch_buff << "." << endl;	//pour test
 		Lexeme *lex_buff = new Lexeme(ch_buff,ligne,0);
 
 		if(verif(*lex_buff,&is_com,&ligne_com))
@@ -57,21 +58,22 @@ void stock_lexemes(string buffer, int ligne, list<Lexeme*>& Lex_VHDL)
 
 		Lex_VHDL.push_back(lex_buff);
 		ch_buff = strtok(NULL," ");
+
 	}
 }
 
-//Fonction pour lire une ligne dans un fichier et lui ajouter des espaces entre les caractÃÂ¨res
-//Retourne la ligne lue et formatÃÂ©e
+//Fonction pour lire une ligne dans un fichier et lui ajouter des espaces entre les caracteres
+//Retourne la ligne lue et formatee
 string read_line(ifstream& fichier)
 {
-	//RÃÂ©cupÃÂ©ration de la ligne
+	//Recuperation de la ligne
 	string buffer="";
-	getline(fichier, buffer);	//traitement d'une ligne ÃÂ  chaque boucle
-	buffer+="\n"; //on rajoute un espace pour bien ÃÂªtre sÃÂ»r de sÃÂ©parer la fin de la ligne du dÃÂ©but de la ligne suivante  
+	getline(fichier, buffer);	//traitement d'une ligne a chaque boucle
+	buffer+="\n"; //on rajoute un espace pour bien etre sur de separer la fin de la ligne du debut de la ligne suivante  
 	//cout << buffer << endl;	//pour test
 
 	//Reformatage de la ligne
-	minus_string(buffer);		//CaractÃÂ¨res passÃÂ©s en miniscule
+	minus_string(buffer);		//Caracteres passes en miniscule
 	//buffer = add_space_on_string(buffer, buffer.length());	//Ajout des espaces
 	//cout << buffer << endl;	//pour test
 	
@@ -79,48 +81,51 @@ string read_line(ifstream& fichier)
 }
 
 //Fonction pour ajouter des espaces sur un string
-//Retourne un nouveau string reformatÃÂ©
+//Retourne un nouveau string reformate
 string add_space_on_string(string str, int length)
 {
-	int i=0;		//incrÃÂ©ment pour str
-	char temp[length*2];	//pour la recopie : dans le pire cas, on ajoute un espace entre chaque caractÃÂ¨re, donc length*2
-	int j=0;		//incrÃÂ©ment pour temp
+	int i=0;		//increment pour str
+	char temp[length*2];	//pour la recopie : dans le pire cas, on ajoute un espace entre chaque caractere, donc length*2
+	int j=0;		//increment pour temp
 
-	while(str[i]!='\0')	//boucle tant qu'on n'est pas arrivÃÂ© ÃÂ  la fin du string
+	while(str[i]!='\0')	//boucle tant qu'on n'est pas arrive a la fin du string
 	{
-		if(str[i]=='	'||str[i]=='\n')	// la tabulation et le saut de ligne sont traitÃÂ©s comme un espace "classique"
+		if(str[i]=='	'||str[i]=='\n')	// la tabulation et le saut de ligne sont traites comme un espace "classique"
 		{
 			str[i]=' ';
 		}
 
-		if(j==0)	// Premier caractÃÂ¨re recopiÃÂ© : le premier caractÃÂ¨re non-espace
+		if(j==0)	// Premier caractere recopie : le premier caractere non-espace
 		{
 			if(!(str[i]==' '))
 			{
 				temp[j]=str[i];
 				j++;
 
-				if(is_ponctuation2char(str[i],str[i+1]))	//Si le lexÃÂ¨me est une ponctuation ÃÂ  2 chars : on recopie le 2e caractÃÂ¨re
+				if(is_ponctuation2char(str[i],str[i+1]))	//Si le lexeme est une ponctuation a 2 chars : on recopie le 2e caractere
 				{
 					i++;
 					temp[j]=str[i];
 					j++;
 				}
 
-				if(is_ponctuation(str[i])&&!(str[i+1]==' '))	//Si ponctuation & pas d'espace aprÃÂ¨s : on rajoute un espace
+				if(is_ponctuation(str[i])&&(str[i+1]!=' '))	//Si ponctuation & pas d'espace apres : on rajoute un espace si on est pas Ã  la fin de la chaÃ®ne
 				{
-					temp[j]=' ';
-					j++;
+					if(str[i+1]!='\0')
+					{
+						temp[j]=' ';
+						j++;
+					}
 				}
 			}
 		}
-		else		// CaractÃÂ¨res suivants
+		else		// Caracteres suivants
 		{
 			if(is_ponctuation(str[i]))	//si ponctuation
 			{
-				//Recopie du premier caractÃÂ¨re
-
-				if(temp[j-1]==' ')	//si on a recopiÃÂ© un espace auparavent, on ne recopie que la ponctuation
+				//Recopie du premier caractere
+				
+				if(temp[j-1]==' ')	//si on a recopie un espace auparavent, on ne recopie que la ponctuation
 				{
 					temp[j]=str[i];
 					j++;
@@ -133,37 +138,37 @@ string add_space_on_string(string str, int length)
 					j++;
 				}
 
-				//Tests : rajouter un espace ? Recopier un deuxiÃÂ¨me caractÃÂ¨re ?
+				//Tests : rajouter un espace ? Recopier un deuxieme caractere ?
 
-				if(is_ponctuation2char(str[i], str[i+1]))	//si les 2 ponctuations qui se suivent sont une affectation ou un commentaire, on recopie directement le caractÃÂ¨re suivant
+				if(is_ponctuation2char(str[i], str[i+1]))	//si les 2 ponctuations qui se suivent sont une affectation ou un commentaire, on recopie directement le caractere suivant
 				{
-					i++;		//caractÃÂ¨re suivant
+					i++;		//caractere suivant
 					temp[j]=str[i]; //on recopie
 					j++;
-
 				}
 
-				if(str[i+1]!=' ') // si le caractÃÂ¨re suivant le lexÃÂ¨me n'est pas un espace, on en rajoute un
+				if((str[i+1]!=' ')&&(str[i+1]!='\0')) // si le caractere suivant le lexeme n'est pas un espace ou une fin de chaÃ®ne, on en rajoute un
 				{
+					cout << "On rajoute un espace" << endl;
 					temp[j]=' ';
 					j++;
 				}
 			}
 			else	//si pas ponctuation
 			{
-				if(!(str[i]==' '&&temp[j-1]==' '))	//si on a dÃÂ©jÃÂ  stockÃÂ© un espace prÃÂ©cÃÂ©demment, on n'en stocke plus
+				if(!(str[i]==' '&&temp[j-1]==' '))	//si on a dejaÂ  stocke un espace precedemment, on n'en stocke plus
 				{
+					cout << "On recopie " << str[i] << endl;
 					temp[j]=str[i];			//dans tous les autres cas, on recopie
 					j++;
 				}
 			}
 		}
-		i++;	//on passe ÃÂ  la recopie du caractÃÂ¨re suivant
+		i++;	//on passe a la recopie du caractere suivant
 	}
-	temp[j]='\0';	//sortie de la boucle = fin de chaÃÂ®ne, on le signale dans temp
+	temp[j]='\0';	//sortie de la boucle = fin de chaine, on le signale dans temp
 
-	//cout << str << endl;		//Pour le test
-	//cout << temp << endl;
+	cout << "ChaÃ®ne stockee :" << temp << endl;		//Pour le test
 	str = temp;
 	return str;
 }
