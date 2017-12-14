724dfb382e2cefa8290fd4ddb096ab23beb65bec
phelma2app <31961914+phelma2app@users.noreply.github.com>
2017-12-14 22:32:13 +0100

    Update Console.cpp

--- Source/Console.cpp
+++ Source/Console.cpp
@@ -2,23 +2,26 @@
 
 //Commandes sur la console pour l'utilisateur
 //Retourne un entier qui vaut 0 si erreur, sinon il renvoie une valeur associee a la commande
+
+//Compare deux fichiers et indique s'ils sont differents (permet de vérifier les changements apportés par l'arbre
 int commande_compare(string &fichier1, string &fichier2)
 {
 	cin >> fichier1;
-    cin >> fichier2;
-    return COMPARE;
+	cin >> fichier2;
+	return COMPARE;
 }
 
 int commande_compare(string &fichier1, string &fichier2, list<string> commandes, list<string>::iterator itr)
 {
-   	itr++;
-    fichier1=(*itr);
-    itr++;
-    fichier2=(*itr);
+	itr++;
+	fichier1=(*itr);
+	itr++;
+	fichier2=(*itr);
 
-    return COMPARE;
+	return COMPARE;
 }
 
+//Interprete les commandes rentrees par l'utilisateur
 int commande_vhdl(string &libraryname, string &sourcename)
 {
 	string commande;
@@ -96,15 +99,16 @@ int commande_vhdl(string &libraryname, string &sourcename, list<string> commande
 }
 
 
-//Commande VHDLCOMP
-//
+//Compilation d'un fichier VHDL
+//Optionnel : on peut rajouter le nom d'un fichier librairie par -l
+//Dans ce cas l'arbre sera sauvegarde dans ce fichier
 int commande_vhdlcomp(string &libraryname, string &sourcename, list<string> commandes, list<string>::iterator itr)
 {
 	//cout << "On va compiler" << endl;	//pour test
 	itr ++;
 	if((*itr)=="-l")
 	{
-		cout << "Le nom de la librarie est donnee" << endl;	//pour test
+		//cout << "Le nom de la librarie est donnee" << endl;
 		itr++;
 		libraryname=(*itr);
 		itr++;
@@ -112,7 +116,7 @@ int commande_vhdlcomp(string &libraryname, string &sourcename, list<string> comm
 	}
 	else
 	{
-		cout << "Le nom de la librairie n'est pas donnee" << endl; //pour test
+		//cout << "Le nom de la librairie n'est pas donnee" << endl; //pour test
 		libraryname="";
 		sourcename=(*itr);
 	}
@@ -126,22 +130,23 @@ int commande_vhdlcomp(string &libraryname, string &sourcename)
 	cin >> commande;
 	if(commande=="-l")
 	{
-		cout << "Le nom de la librarie est donnee" << endl;	//pour test
+		//cout << "Le nom de la librarie est donnee" << endl;	//pour test
 		cin >> libraryname;
 		cin >> sourcename;
 	}
 	else
 	{
-		cout << "Le nom de la librairie n'est pas donnee" << endl; //pour test
+		//cout << "Le nom de la librairie n'est pas donnee" << endl; //pour test
 		libraryname="";
 		sourcename=commande;
 	}
 	return VHDLCOMP;
 }
 
+//Permet de lire un fichier script comprenant les differentes commandes
 int commande_source(string &sourcename, list<string> commandes, list<string>::iterator itr)
 {
-	cout << "On va executer un script" << endl;	//pour test
+	//cout << "On va executer un script" << endl;	//pour test
 	itr++;
 	sourcename=(*itr);
 	return SOURCE;
@@ -149,11 +154,12 @@ int commande_source(string &sourcename, list<string> commandes, list<string>::it
 
 int commande_source(string &sourcename)
 {
-	cout << "On va executer un script" << endl;	//pour test
+	//cout << "On va executer un script" << endl;	//pour test
 	cin >> sourcename;
 	return SOURCE;
 }
 
+//Permet d'executer les differentes commandes contenues dans un script
 int execute_script(string nom_script)
 {
 	cout << "--Execute script " << nom_script << "--" << endl;
@@ -179,7 +185,7 @@ int execute_script(string nom_script)
 		commandes.push_back(buffer);
 	}
 
-	//cout << "Commande vhdl va etre lance" << endl;	//Pour test
+    //Traitement des commandes
 	list<string>::iterator itr=commandes.begin();
 	bool err_tree=false;
 	int err_pars;
@@ -198,30 +204,37 @@ int execute_script(string nom_script)
                 break;
 			case VHDLCOMP :
 				lex.clear();
-				vhdlcomp(libraryname,sourcename,lex);
-				cout << endl << "--Parseur en cours--" << endl;
-				err_pars=parseur_root(lex);
-				print_lex(lex);
+				if(!vhdlcomp(libraryname,sourcename,lex))
+                {
+                    cout << endl << "--Parseur en cours--" << endl;
+                    err_pars=parseur_root(lex);
+                    print_lex(lex);
 
-				if(err_pars==1)
-				{
-					cout << endl << "--Creation de l'arbre--" << endl;
-					parseur = createTree(lex,err_tree);
-					cout << endl;
-					if(!err_tree)
+                    if(err_pars==1)
                     {
-                        cout << endl << "--Impression de l'arbre--" << endl;
-                        printTree(parseur);
-                        if(libraryname!="")
-                            saveTree(parseur,libraryname);
+                        cout << endl << "--Creation de l'arbre--" << endl;
+                        parseur = createTree(lex,err_tree);
+                        cout << endl;
+                        if(!err_tree)
+                        {
+                            cout << endl << "--Impression de l'arbre--" << endl;
+                            printTree(parseur);
+                            if(libraryname!="")
+                                saveTree(parseur,libraryname);
+                        }
+                        cout << endl << "--Fin de compilation--" << endl << endl;
                     }
-					cout << endl << "--Fin de compilation--" << endl << endl;
-				}
-				else
-				{
-					cout << endl << "--Erreur de syntaxe--" << endl;
-					cout <<  "--Compilation avortée--" << endl << endl;
-				}
+                    else
+                    {
+                        cout << endl << "--Erreur de syntaxe--" << endl;
+                        cout <<  "--Compilation avortée--" << endl << endl;
+                    }
+                }
+                else
+                {
+                    cout << endl << "--Erreur orthographique--" << endl;
+                    cout <<  "--Compilation avortée--" << endl << endl;
+                }
 				break;
 			case SOURCE:
 				execute_script(sourcename);
