8e128fd41409c6a0d4be2e10bb5d1ff4c70faf5e
phelma2app <31961914+phelma2app@users.noreply.github.com>
2017-12-14 22:29:56 +0100

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -12,26 +12,32 @@ using namespace std;
 
 int main()
 {
+    //Fonctionnement
 	string libraryname;
 	string sourcename;
 	list<Lexeme*> lex;
 	tree<Lexeme*> parseur;
+	bool close=false;
+
+    //Erreurs
 	int err_pars=0;
-	bool err_tree=false;
+	bool err_lex=false,err_tree=false;
+
 	cout << endl << "---Synthetiseur VHDL Beta 1.0---" << endl << endl;
 
-	bool close=false;
+//Boucle principale
 	while(!close)
 	{
 		cout << "--Ready--" << endl;
 		int commande=commande_vhdl(libraryname,sourcename);
-		while(commande==ERREUR)
+
+		while(commande==ERREUR) //Si une commande n'est pas reconnue : on en demande une nouvelle
 		{
 			commande=commande_vhdl(libraryname,sourcename);
 		}
 
         int nligne=0;
-		switch(commande)
+		switch(commande)    //Lance les differentes commandes recues
 		{
             case COMPARE :
                 if(fichiers_identiques(sourcename,libraryname, nligne))
@@ -45,30 +51,37 @@ int main()
 				break;
 			case VHDLCOMP :
 				lex.clear();
-				vhdlcomp(libraryname,sourcename,lex);
-				cout << endl << "--Parseur en cours--" << endl;
-				err_pars=parseur_root(lex);
-				print_lex(lex);
+				if(!vhdlcomp(libraryname,sourcename,lex))
+                {
+                    cout << endl << "--Parseur en cours--" << endl;
+                    err_pars=parseur_root(lex);
+                    print_lex(lex);
 
-				if(err_pars==1)
-				{
-					cout << endl << "--Creation de l'arbre--" << endl;
-					parseur = createTree(lex,err_tree);
-					cout << endl;
-					if(!err_tree)
+                    if(err_pars==1)
                     {
-                        cout << endl << "--Impression de l'arbre--" << endl;
-                        printTree(parseur);
-                        if(libraryname!="")
-                            saveTree(parseur,libraryname);
+                        cout << endl << "--Creation de l'arbre--" << endl;
+                        parseur = createTree(lex,err_tree);
+                        cout << endl;
+                        if(!err_tree)
+                        {
+                            cout << endl << "--Impression de l'arbre--" << endl;
+                            printTree(parseur);
+                            if(libraryname!="")
+                                saveTree(parseur,libraryname);
+                        }
+                        cout << endl << "--Fin de compilation--" << endl << endl;
                     }
-					cout << endl << "--Fin de compilation--" << endl << endl;
-				}
-				else
-				{
-					cout << endl << "--Erreur de syntaxe--" << endl;
-					cout <<  "--Compilation avortée--" << endl << endl;
-				}
+                    else
+                    {
+                        cout << endl << "--Erreur de syntaxe--" << endl;
+                        cout <<  "--Compilation avortée--" << endl << endl;
+                    }
+                }
+                else
+                {
+                    cout << endl << "--Erreur orthographique--" << endl;
+                    cout <<  "--Compilation avortée--" << endl << endl;
+                }
 				break;
 			default: cout << endl << "--Sortie de programme--" << endl << endl;
 				close=true;
